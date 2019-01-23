7f610651e2619919fde950fdcb26f22acdfb27d4
HikkaV <urumipainblackreaper@gmail.com>
2019-01-23 22:22:00 +0200

    eee

--- Main.py
+++ Main.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     url = Settings.url
     path = Settings.path
     key = int(input())
+    helpers.make_dir(path)
     firstlen = len(os.listdir(path))
     parser = Parser(key, w, b, num_px, path, firstlen)
-    helpers.make_dir(path)
     pool = Pool(processes=helpers.define_sitesqua_to_parse())
     pool.map(parser.parse, url)
     print("The " + str(len(os.listdir(path)) - firstlen) + " pics were downloaded")
--- Settings.py
+++ Settings.py
@@ -1,5 +1,5 @@
 url = ['https://www.pexels.com/search/car/', 'https://stocksnap.io/search/car']  # used sites
-path = '/home/hikkav/environments/my_env/mytraindata'  # path to save images to
+path = '/home/hikkav/environments/my_env/traindata'  # path to save images to
 qua_sites = len(url)
 adress = '/home/hikkav/environments/my_env/train.hdf5'  # address to where you want to save the hdf5 file while preprocessing the data
 path_to_car = '/home/hikkav/environments/my_env/train/*.jpg'  # path where the one saved dataset
